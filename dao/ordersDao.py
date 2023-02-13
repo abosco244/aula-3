543ad9e84fdb8b33b6723e73782479d47546de3d
Borrellsss <edoardofilippo.borrello@gmail.com>
2023-02-13 15:40:13 +0100

    add getAllOrdersByCustomerNumber method to ordersDao

---
+++ dao/ordersDao.py
@@ -0,0 +1,18 @@
+from dao.utility.db import MySql
+from models.order import OrderModel
+
+class OrdersDao:
+    
+    @classmethod
+    def getAllOrdersByCustomerNumber(cls, customer_number):
+        MySql.openConnection()
+        MySql.query(
+            f'''SELECT *
+                FROM orders o
+                o.customerNumber = {customer_number}'''
+        )
+        rows = MySql.getResults()
+        MySql.closeConnection()
+
+        for row in rows:
+            pass
