10a380438c0214f195f6d2934fdc459b1b57d35c
DEV1 - davidefiabane <davide.fiabane@gmail.com>
2023-02-14 11:23:07 +0100

    office model def

--- models/office.py
+++ models/office.py
@@ -1,5 +1,4 @@
-
-<<<<<<< HEAD
+from pydantic import BaseModel
 class OfficeModel (BaseModel):
   officeCode : str 
   city : str
@@ -10,17 +9,3 @@ class OfficeModel (BaseModel):
   country : str
   postalCode : str
   territory : str
-=======
-from pydantic import BaseModel
-    
-class OfficeModel (BaseModel):
-      officeCode : str 
-      city : str
-      phone : str
-      addressLine1 : str
-      addressLine2 : str | None = None
-      state : str | None = None
-      country : str
-      postalCode : str
-      territory : str
->>>>>>> 0637f5ee41af4809176a890894f21e896fff6ee7
